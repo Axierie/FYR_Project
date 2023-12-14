ddd9addf5a385bab38eb7c0f061cf5e93fedf363
Axierie <124911244+Axierie@users.noreply.github.com>
2023-12-14 13:41:55 +0000

    Update example.py

--- example.py
+++ example.py
@@ -8,9 +8,7 @@ from sklearn.metrics import r2_score
 from fs_method import evaluate_model
 
 #LOAD DATA
-test = pd.read_csv(f'{os.getcwd()}\Datasets\Subset2_Test_Set.csv')
-x_test=test[test.columns.values].drop(columns="lower flammability limit (vol%)") 
-y_test=test[['lower flammability limit (vol%)']]
+test = pd.read_csv('MixedDescriptorFileExample.csv')
 
 #LOAD SCALER AND MODEL
 scx = joblib.load('Mixed_Subset_Feature_Scaler.pkl')
@@ -20,7 +18,7 @@ model = pickle.load(open('Mixed_LASSO_model.sav', 'rb'))
 #SCALE DATA
 x_test_scaled = scx.transform(x_test)
 y_pred_scaled = model.predict(x_test_scaled)
-y_pred = scy.inverse_transform(np.reshape(y_pred_scaled,(-1,1)))
-print(r2_score(y_test,y_pred))
+y_pred = scy.inverse_transform(np.reshape(y_pred_scaled,(-1,1))) #array of predicted LFLs
+
 
 
