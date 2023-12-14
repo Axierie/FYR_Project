c087b191f27f6d2df35441fa9364135f12925480
Axierie <124911244+Axierie@users.noreply.github.com>
2023-12-14 13:55:07 +0000

    Update example.py

--- example.py
+++ example.py
@@ -1,24 +1,24 @@
 #IMPORT
 import joblib
 import pickle
-import os
 import pandas as pd
 import numpy as np
-from sklearn.metrics import r2_score
-from fs_method import evaluate_model
 
 #LOAD DATA
-test = pd.read_csv('MixedDescriptorFileExample.csv')
+x_test = pd.read_csv(f'MixedDescriptorExample.csv')
 
 #LOAD SCALER AND MODEL
 scx = joblib.load('Mixed_Subset_Feature_Scaler.pkl')
 scy = joblib.load('Mixed_Subset_Target_Scaler.pkl')
-model = pickle.load(open('Mixed_LASSO_model.sav', 'rb'))
+model = pickle.load(open('Mixed_SVR_model.sav', 'rb'))
 
 #SCALE DATA
 x_test_scaled = scx.transform(x_test)
 y_pred_scaled = model.predict(x_test_scaled)
-y_pred = scy.inverse_transform(np.reshape(y_pred_scaled,(-1,1))) #array of predicted LFLs
+y_pred = scy.inverse_transform(np.reshape(y_pred_scaled,(-1,1))) #Predicted data, unscaled
+
+
+
 
 
 
